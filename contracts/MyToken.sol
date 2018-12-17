416924b2bb896c59925fc2f92dfdaf0b723669d8
rene <renegmed@yahoo.com>
2018-12-17 13:37:04 -0500

    section 3 - addling allocation approvals - delegated spender's allowance

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,10 +9,14 @@ contract MyToken {
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value); // indexed make an item searchable
 
+    // This generates a public event on the blockchain that will notify clients
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
     /* Initializes contract with initial supply tokens to the creator of the contract */
     constructor(uint256 initialSupply, string tokenName, string tokenSymbol) public {
         totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
@@ -21,13 +25,67 @@ contract MyToken {
         symbol = tokenSymbol;                                   // Set the symbol for display purposes
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Not enough sender's balance to transfer"); // Check if the sender has enough
-        require(balanceOf[_to] + _value >= balanceOf[_to], "Causes balance overflow");   // Check for overflows
-        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
-        balanceOf[_to] += _value;                             // Add the same to the recipient
-        emit Transfer(msg.sender, _to, _value);               // emit Event
-        return true;
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint256 _value) internal { 
+        // Prevent transfer to 0x0 address.
+        require(_to != 0x0);
+        // Check if the sender has enough         
+        require(balanceOf[_from] >= _value, "Not enough sender's balance to transfer");
+        // Check for overflows 
+        require(balanceOf[_to] + _value >= balanceOf[_to], "Causes balance overflow");
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];   
+        // Subtract from the sender
+        balanceOf[_from] -= _value; 
+        // Add the same amount to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value); // emit Event
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances); 
     }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     *
+     */
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        success = true; 
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     * 
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     *
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender,"Amount request is not covered by remaining allowance");  // Check allowance if requested amount is covered
+        allowance[_from][msg.sender] -= _value;   // reduce the allowance
+        _transfer(_from, _to, _value);
+    }
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `spender` to spend no more than `_value` tokens on your behalf
+     * 
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     *
+     */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); // emit
+        return true;
+    } 
+
 } 
